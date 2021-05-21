b4b37a2266056a41150a07932e37d995254fbf03
newhorizon-tech <alaukikinbox@gmail.com>
2021-05-21 22:29:38 +0530

    Add initial scenes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Javascript is working")
+import Phaser from 'phaser'
+
+import {Scene1} from './scenes/scene-1'
+import {Scene2} from './scenes/scene-2'
+
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: [Scene1, Scene2]
+};
+
+var game = new Phaser.Game(config);
---
+++ src/scenes/scene-1.js
@@ -0,0 +1,21 @@
+export class Scene1 extends Phaser.Scene {
+
+  constructor() {
+    super("Scene1");
+  }
+
+  preload() {
+    this.load.image('boat', 'assets/boat.jpg');
+  }
+
+
+  async create() {
+    this.add.text(100, 100, 'Scene One!', {
+      fill: 'purple'
+    });
+    this.add.image(400, 300, 'boat');
+    await new Promise(r => setTimeout(r, 5000));
+
+    this.scene.start("Scene2")
+  }
+}

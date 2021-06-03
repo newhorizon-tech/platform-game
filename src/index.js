b4f81b0d54b1e27a742d8c8f565adbbf81a9a2af
newhorizon-tech <alaukikinbox@gmail.com>
2021-06-03 13:44:00 +0530

    Modify game config

--- src/index.js
+++ src/index.js
@@ -3,18 +3,30 @@ import Phaser from 'phaser'
 import {Scene1} from './scenes/scene-1'
 import {Scene2} from './scenes/scene-2'
 
-
-var config = {
+let gameConfig = {
     type: Phaser.AUTO,
-    width: 900,
-    height: 700,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 400 }
-        }
-    },
-    scene: [Scene2, Scene1]
-};
+    width: 1334,
+    height: 750,
+    scene: playGame,
+    backgroundColor: blue,
 
-var game = new Phaser.Game(config);
+    // physics settings
+    physics: {
+        default: "arcade"
+    }
+}
+
+// var config = {
+//     type: Phaser.AUTO,
+//     width: 900,
+//     height: 700,
+//     physics: {
+//         default: 'arcade',
+//         arcade: {
+//             gravity: { y: 400 }
+//         }
+//     },
+//     scene: [Scene2, Scene1]
+// };
+
+let game = new Phaser.Game(config);

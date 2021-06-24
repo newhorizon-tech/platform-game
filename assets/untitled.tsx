f1ffab6718ab80139da7bc25e8b146344e3e0619
newhorizon-tech <alaukikinbox@gmail.com>
2021-06-24 21:55:27 +0530

    Add tileset

---
+++ assets/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="JumpingKoala" tilewidth="16" tileheight="16" tilecount="264" columns="24">
+ <image source="kenney_pixelplatformer/Tilemap/tiles.png" width="398" height="178"/>
+</tileset>

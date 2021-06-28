241c58455eda9bac702eb9bfe782dbd73bc76e10
newhorizon-tech <alaukikinbox@gmail.com>
2021-06-28 18:58:44 +0530

    Add collisions

--- src/scenes/scene-1.js
+++ src/scenes/scene-1.js
@@ -8,7 +8,7 @@ export class Scene1 extends Phaser.Scene {
 
   preload() {
     this.load.image('background', 'assets/background.png');
-    this.load.image('koala_idle', 'assets/koala_idle.png');
+    this.load.atlas('player', 'assets/koala/koala.png', 'assets/koala/koala_atlas.json');
     this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
     this.load.tilemapTiledJSON('tilemap','assets/tilemaps/game.json')
 
@@ -19,7 +19,16 @@ export class Scene1 extends Phaser.Scene {
     const backgroundImage = this.add.image(0, 0,'background').setOrigin(0, 0);
     const map = this.make.tilemap({ key: 'tilemap'})
     const tileset = map.addTilesetImage('koala_tileset','tiles', 64, 64)
-    const player = this.physics.add.sprite(100,100,'koala_idle')
+
+    const platform = map.createLayer('platform', tileset, 0, 0);
+    platform.setCollisionByExclusion(-1, true);
+
+    this.player = this.physics.add.sprite(100,100,'player')
+    this.player.setBounce(0.1);
+    this.player.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player, platform);
+
+
 
     // this.add.image(400, 300, 'koala_idle');
     // this.add.image

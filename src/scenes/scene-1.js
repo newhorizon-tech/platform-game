bbdea049b06251fe9685dac5d465b1c9580b4696
newhorizon-tech <alaukikinbox@gmail.com>
2021-07-02 20:13:14 +0530

    Add walk and idle animation for player

--- src/scenes/scene-1.js
+++ src/scenes/scene-1.js
@@ -1,4 +1,6 @@
-import {gameOptions} from './gameOptions'
+import {
+  gameOptions
+} from './gameOptions'
 
 export class Scene1 extends Phaser.Scene {
 
@@ -10,28 +12,47 @@ export class Scene1 extends Phaser.Scene {
     this.load.image('background', 'assets/background.png');
     this.load.atlas('player', 'assets/koala/koala.png', 'assets/koala/koala_atlas.json');
     this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
-    this.load.tilemapTiledJSON('tilemap','assets/tilemaps/game.json')
+    this.load.tilemapTiledJSON('tilemap', 'assets/tilemaps/game.json')
 
   }
 
 
   async create() {
-    const backgroundImage = this.add.image(0, 0,'background').setOrigin(0, 0);
-    const map = this.make.tilemap({ key: 'tilemap'})
-    const tileset = map.addTilesetImage('koala_tileset','tiles')
+    const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
+    const map = this.make.tilemap({
+      key: 'tilemap'
+    })
+    const tileset = map.addTilesetImage('koala_tileset', 'tiles')
 
     const platform = map.createLayer('platform', tileset, 0, 0);
     platform.setCollisionByExclusion(-1, true);
 
-    this.player = this.physics.add.sprite(100,100,'player')
+    this.player = this.physics.add.sprite(100, 100, 'player')
     this.player.setBounce(0.1);
     this.player.setCollideWorldBounds(true);
     this.physics.add.collider(this.player, platform);
 
+    this.anims.create({
+      key: 'walk',
+      frames: this.anims.generateFrameNames('player', {
+        prefix: 'koala_player',
+        start: 2,
+        end: 3,
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'idle',
+      frames: [{
+        key: 'player',
+        frame: 'koala_player_0'
+      }],
+      frameRate: 10,
+    });
 
 
-    // this.add.image(400, 300, 'koala_idle');
-    // this.add.image
 
     // await new Promise(r => setTimeout(r, 5000));
 

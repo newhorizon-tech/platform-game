99d2e71bfd062f9694a9d025c7ba6f1e04991ba3
newhorizon-tech <alaukikinbox@gmail.com>
2021-07-04 20:01:29 +0530

    Add spikes' sprites

--- src/scenes/scene-1.js
+++ src/scenes/scene-1.js
@@ -25,6 +25,8 @@ export class Scene1 extends Phaser.Scene {
     const tileset = map.addTilesetImage('koala_tileset', 'tiles')
 
     const platform = map.createLayer('platform', tileset, 0, 0);
+    const obsctacles = map.createLayer('obstacles', tileset, 0, 0);
+
     platform.setCollisionByExclusion(-1, true);
 
     this.player = this.physics.add.sprite(100, 100, 'player')
@@ -63,7 +65,29 @@ export class Scene1 extends Phaser.Scene {
       frameRate: 10,
     });
 
+    // Create a sprite group for all spikes, set common properties to ensure that
+    // sprites in the group don't move via gravity or by player collisions
+    this.spikes = this.physics.add.group({
+      allowGravity: false,
+      immovable: true
+    });
 
+    const objectLayer = map.getObjectLayer('objects');
+
+    // const spikobjectLayer.objects['spikes']
+
+    // Let's get the spike objects, these are NOT sprites
+    const spikeObjects = objectLayer.objects["spikes"]
+    console.log(spikeObjects);
+  //
+  //   // Now we create spikes in our sprite group for each object in our map
+    spikeObjects.forEach(spikeObject => {
+      // Add new spikes to our sprite group, change the start y position to meet the platform
+      const spike = this.spikes.create(spikeObject.x, spikeObject.y + 200 - spikeObject.height, 'spike').setOrigin(0, 0);
+    });
+  //
+  //
+  //
   }
 
   update() {
